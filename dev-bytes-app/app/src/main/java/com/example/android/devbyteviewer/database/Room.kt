c1da8661b26768284e9985d18ed68cdb56f10af1
Andrew Weiss <andreivayss@gmail.com>
2021-11-02 08:39:13 +0700

    added the video database

--- dev-bytes-app/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ dev-bytes-app/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -1,15 +1,36 @@
 package com.example.android.devbyteviewer.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface VideoDao {
+    /**
+     * When we return the LiveData, room will do a database query in a background for us
+     * It will update the LiveData anytime the new data is written in the table
+     */
     @Query("select * from databasevideo")
-    fun getVideos(): List<DatabaseVideo>
+    fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
 }
